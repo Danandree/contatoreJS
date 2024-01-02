112df57943145268284aad30022ad01ff2ff18b5
Daniele Andreetta <danieleandree91@gmail.com>
2024-01-02 11:27:07 +0100

    commit

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,82 +2,112 @@ let contatore = 0;
 let inizio = 0;
 let incremento = 1;
 let precision = 0;
+let numeroContatore;
+let btnContainer;
 
-document.getElementById("numeroContatore").innerHTML = inizio;
-document.getElementById("numeroIncremento").innerHTML = incremento;
-document.getElementById("numeroIniziale").innerHTML = inizio;
+const container = document.getElementsByClassName("container")[0];
+const numeroIncremento = document.getElementById("numeroIncremento");
+const numeroIniziale = document.getElementById("numeroIniziale");
+const settingsButton = document.getElementById("settingsButton");
+const setContainer = document.getElementById("setContainer");
 
-document.getElementById("aumenta").onclick = () => {
-    contatore += incremento;
-    updateNumber();
+settingsButton.onclick = () => openSettings();  
+container.onclick = () => updateNumber();
+
+class settingsMenuListener{
+    constructor(elemento){
+        this._elemento = elemento;
+        elemento.onclick = this.onClick.bind(this);
+    }
+
+    setInitialNumber(){
+        let nuovoInizio = prompt("Immettere il nuovo numero di parternza del contatore");
+        if(Number(nuovoInizio)){
+            this.updateInizio(parseFloat(nuovoInizio));
+        }
+        else{
+            alert("\"" + nuovoInizio + "\" non é un numero valido")
+        }
+    }
+    resetInitialNumber(){
+        this.updateInizio(0);
+    }
+    setNumber(){
+        let nuovoIncremento = prompt("Immettere il nuovo numero di incremento");
+        if(Number(nuovoIncremento)){
+            let decimalStr = nuovoIncremento.toString().split('.')[1];
+            if(decimalStr != undefined){
+                this.updateIncremento(parseFloat(nuovoIncremento), decimalStr.length);
+            }
+            else{
+                this.updateIncremento(parseFloat(nuovoIncremento), 0);
+            }
+        }
+        else{
+            alert("\"" + nuovoIncremento + "\" non é un numero valido")
+        }
+    }
+    resetNumber (){
+        this.updateIncremento(1,0);
+    }
+    updateIncremento(uInc, prec){
+        incremento = uInc;
+        precision = prec;
+    }
+    updateInizio(ini){
+        inizio = ini;
+        contatore = ini;
+    }
+    onClick(event){
+        let azione = event.target.dataset.action;
+        if(azione){
+            this[azione]();
+        }
+    }
 }
-
-document.getElementById("resetta").onclick = () => {
-    contatore = inizio;
-    updateNumber();
-}
-
-document.getElementById("diminuisci").onclick = () => {
-    contatore -= incremento;
-    updateNumber();
-} 
-
-document.getElementById("settingsButton").onclick = () => openSettings();  
-document.getElementById("setNumeroIniziale").onclick = () => cambiaNumeroIniziale();
-document.getElementById("setNumeroIncremento").onclick = () => cambiaIncremento();
-document.getElementById("resetNumeroIncremento").onclick = ()=> updateIncremento(1,0);
-document.getElementById("resetNumeroIniziale").onclick = ()=> updateInizio(0);
-
 function updateNumber(){
-    document.getElementById("numeroContatore").innerHTML = contatore.toFixed(precision);
-    document.getElementById("numeroIniziale").innerHTML = inizio;
-    document.getElementById("numeroIncremento").innerHTML = incremento;
+    numeroContatore.innerHTML = contatore.toFixed(precision);
+    numeroIniziale.innerHTML = inizio;
+    numeroIncremento.innerHTML = incremento;
 }
 
 function openSettings(){
-    if(document.getElementById("setContainer").style.display === "none" || document.getElementById("setContainer").style.display === ""){
-        document.getElementById("setContainer").style.display = "flex";
-        document.getElementById("settingsButton").innerHTML = "Chiudi parametri";
+    if(setContainer.style.display === "none" || setContainer.style.display === ""){
+        setContainer.style.display = "flex";
+        settingsButton.innerHTML = "Chiudi parametri";
     }
     else{
-        document.getElementById("setContainer").style.display = "none";
-        document.getElementById("settingsButton").innerHTML = "Cambia parametri";
+        setContainer.style.display = "none";
+        settingsButton.innerHTML = "Cambia parametri";
     }
 }
 
-function cambiaNumeroIniziale(){
-    let nuovoInizio = prompt("Immettere il nuovo numero di parternza del contatore");
-    if(Number(nuovoInizio)){
-        updateInizio(parseFloat(nuovoInizio));
-    }
-    else{
-        alert("\"" + nuovoInizio + "\" non é un numero valido")
-    }
+function createElement(elemento, classe, inHtml){
+    let nuovoElemento = document.createElement(elemento);
+    nuovoElemento.className = classe;
+    nuovoElemento.innerHTML = inHtml;
+    return nuovoElemento;
 }
 
-function cambiaIncremento(){
-    let nuovoIncremento = prompt("Immettere il nuovo numero di incremento");
-    if(Number(nuovoIncremento)){
-        let decimalStr = nuovoIncremento.toString().split('.')[1];
-        if(decimalStr != undefined){
-            updateIncremento(parseFloat(nuovoIncremento), decimalStr.length);
+function makeElements(){
+    let titolo = createElement("h1", "titolo", "Contatore", container);
+    container.insertBefore(titolo, settingsButton);
+    numeroContatore = createElement("label", "numeroContatore", "", container);
+    container.insertBefore(numeroContatore, settingsButton);
+    btnContainer = createElement("div", "btnContainer", "", container);
+    container.insertBefore(btnContainer, settingsButton);
+    const operators = {
+        "-" : () => contatore -= incremento,
+        "Reset" : () => contatore = inizio,        
+        "+" : () => contatore += incremento
         }
-        else{
-            updateIncremento(parseFloat(nuovoIncremento), 0);
-        }
+    for (key in operators){
+        let button = createElement("div", "btn", key);
+        btnContainer.appendChild(button);
+        button.onclick = (parametro) => {operators[parametro.target.innerHTML]()};
     }
-    else{
-        alert("\""+nuovoIncremento+"\" non é un numero valido")
-    }
-}
-function updateIncremento(uInc, prec){
-    incremento = uInc;
-    precision = prec;
-    updateNumber();
 }
 
-function updateInizio(ini){
-    inizio = ini;
-    contatore = ini;
-    updateNumber();
-}
+makeElements();
+updateNumber();
+new settingsMenuListener(setContainer);
